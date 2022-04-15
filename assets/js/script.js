9b440ef78a0d53580de5343e68301595fb41c9ca
Fauzan Taqiyuddin <kelasweekend.id@gmail.com>
2022-04-16 01:08:35 +0700

    Tahap Kedua

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,11 @@ $(document).ready(function () {
     $("#dua").click(function () {
         $('#screen1').addClass('d-none');
         $('#screen2').addClass('d-none');
-        $(".home").removeClass("d-none").addClass("animate__animated animate__slideInDown");
+        // $(".home").removeClass("d-none").addClass("animate__animated animate__slideInDown");
+        $(".home").removeClass("d-none");
+        $(".BawahMobile").addClass("animate__animated animate__slideInUp animate__slow");
+        $(".ImageMobileHeader").addClass("animate__animated animate__bounceInDown animate__slow");
+        $(".btn-back").addClass("animate__animated animate__lightSpeedInRight animate__slow");
     });
     $("#satu").click(function () {
         $('#screen1').removeClass('d-none').addClass("animate__animated animate__slideInUp");
