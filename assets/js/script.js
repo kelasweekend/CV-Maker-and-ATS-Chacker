c2f603374dca5f4326add38245e75e38e1d65aac
Fauzan Taqiyuddin <kelasweekend.id@gmail.com>
2022-04-16 13:44:39 +0700

    Tahap Kedua Revisi

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,16 @@
 $(document).ready(function () {
-    $("#dua").click(function () {
+    $(".start").click(function () {
         $('#screen1').addClass('d-none');
-        $('#screen2').addClass('d-none');
         // $(".home").removeClass("d-none").addClass("animate__animated animate__slideInDown");
         $(".home").removeClass("d-none");
+        $("#navku").removeClass("d-none");
         $(".BawahMobile").addClass("animate__animated animate__slideInUp animate__slow");
-        $(".ImageMobileHeader").addClass("animate__animated animate__bounceInDown animate__slow");
+        $(".ImageMobile").addClass("animate__animated animate__bounceInDown animate__slow");
         $(".btn-back").addClass("animate__animated animate__lightSpeedInRight animate__slow");
     });
     $("#satu").click(function () {
         $('#screen1').removeClass('d-none').addClass("animate__animated animate__slideInUp");
-        $('#screen2').removeClass('d-none').addClass("animate__animated animate__slideInUp");
         $(".home").addClass("d-none");
+        $("#navku").addClass("d-none");
     });
-
-    const navItems = document.getElementsByClassName('hp-item');
-    for (let i = 0; i < navItems.length; i++) {
-        navItems[i].addEventListener('click', () => {
-            for (let j = 0; j < navItems.length; j++)
-                navItems[j].classList.remove('active');
-
-            navItems[i].classList.add('active');
-        });
-    }
 });
