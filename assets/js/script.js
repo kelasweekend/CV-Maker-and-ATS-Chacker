5043fe30e5e0cc35dadc0095927bbe8bdd696401
Fauzan Taqiyuddin <kelasweekend.id@gmail.com>
2022-04-17 00:03:10 +0700

    Tahap Kedua Revisi

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,14 @@
 $(document).ready(function () {
-    $(".start").click(function () {
-        $('#screen1').addClass('d-none');
+    $('.start').click(function () {
+        $('.header-mobile').addClass('d-none');
         // $(".home").removeClass("d-none").addClass("animate__animated animate__slideInDown");
-        $(".home").removeClass("d-none");
-        $("#navku").removeClass("d-none");
-        $(".BawahMobile").addClass("animate__animated animate__slideInUp animate__slow");
-        $(".ImageMobile").addClass("animate__animated animate__bounceInDown animate__slow");
-        $(".btn-back").addClass("animate__animated animate__lightSpeedInRight animate__slow");
+        $('.home').removeClass("d-none");
+        $('.BawahMobile').addClass("animate__animated animate__slideInUp animate__slow");
+        $('.ImageMobile').addClass("animate__animated animate__bounceInDown animate__slow");
+        $('.btn-back').addClass("animate__animated animate__lightSpeedInRight animate__slow");
     });
-    $("#satu").click(function () {
-        $('#screen1').removeClass('d-none').addClass("animate__animated animate__slideInUp");
-        $(".home").addClass("d-none");
-        $("#navku").addClass("d-none");
+    $('.back').click(function () {
+        $('.header-mobile').removeClass('d-none').addClass("animate__animated animate__slideInUp");
+        $('.home').addClass("d-none");
     });
 });
